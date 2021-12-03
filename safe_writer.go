9c5744cbebd594e46658510c1cb7ab15891a2927
Evgeny Abramovich <evg.abramovitch@gmail.com>
2021-12-03 18:06:23 +0300

    Simplify writers

---
+++ safe_writer.go
@@ -0,0 +1,25 @@
+package linebyline
+
+import (
+	"io"
+	"sync"
+)
+
+type safeWriter struct {
+	mu  sync.Mutex
+	out io.Writer
+}
+
+func NewSafeWriter(out io.Writer) *safeWriter {
+	return &safeWriter{
+		mu:  sync.Mutex{},
+		out: out,
+	}
+}
+
+func (wr *safeWriter) Write(payload []byte) (int, error) {
+	wr.mu.Lock()
+	defer wr.mu.Unlock()
+
+	return wr.out.Write(payload)
+}
